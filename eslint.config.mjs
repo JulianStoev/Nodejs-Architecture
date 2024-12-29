3fc27086f0f97ad84d294a0461a5aa4e7d22f5dd
Julian Stoev <stoev60@gmail.com>
2024-12-29 23:08:37 +0200

    chore: add ESLint configuration and update dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            "indent": ["error", 4, { "SwitchCase": 1 }],
+            "typescript-eslint/no-explicit-any": 0,
+            "typescript-eslint/no-unused-vars": 1
+        }
+    }
+];
