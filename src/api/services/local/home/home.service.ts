59c2c1c7dd11513c8494c020d78d5b86b22aec43
Julian Stoev <stoev60@gmail.com>
2025-01-17 23:34:29 +0200

    fix: correct import path for getHomeDb in home.service

--- src/api/services/local/home/home.service.ts
+++ src/api/services/local/home/home.service.ts
@@ -1,4 +1,4 @@
-import { getHomeDb } from "../../../dal/mysql/home.dal";
+import { getHomeDb } from "../../../../dal/mysql/home.dal";
 
 export function getHome(): Promise<any> {
     return getHomeDb();
